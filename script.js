e7b89bd526b6eccedbac94f01732cfe53f84103b
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-28 16:29:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-
+let yourArray = [
+	[
+		{
+			name:"Manisha",
+			age:23
+		}
+	],
+	[
+		{
+			isAlive:true,
+			helpful:true
+		}
+	],
+	[
+		height:150
+	],
+	[
+		{		
+			name:"Radha",
+			age:30
+		}
+	],
+	[
+		{
+		      c: "c",
+		      d: "d"
+	    }	
+	]
+];
